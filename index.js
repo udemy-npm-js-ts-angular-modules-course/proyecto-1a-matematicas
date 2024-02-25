5ba1669ba24d22036c0add33e13d4cdbc76b625d
Gustavo Conturzo <gconturzo@gmail.com>
2024-02-25 18:35:45 -0300

    Primera versión del módulo de matemáticas

---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+    /**
+     * Suma de dos números
+     * @example
+     * n1 = 1, n2 = 3 => 4
+     * @param {*} n1 Número uno de la suma
+     * @param {*} n2 Número dos de la suma
+     * @returns n1 + n2
+     */
+    suma: function(n1, n2) {
+        return n1 + n2;
+    },
+        /**
+     * Resta de dos números
+     * @example
+     * n1 = 1, n2 = 3 => -2
+     * @param {*} n1 Número uno de la resta
+     * @param {*} n2 Número dos de la resta
+     * @returns n1 - n2
+     */
+    resta: function(n1, n2) {
+        return n1 - n2;
+    },
+        /**
+     * Multiplicación de dos números
+     * @example
+     * n1 = 1, n2 = 3 => 3
+     * @param {*} n1 Número uno de la multiplicación
+     * @param {*} n2 Número dos de la multiplicación
+     * @returns n1 * n2
+     */
+    multiplicacion: function(n1, n2) {
+        return n1 * n2;
+    },
+        /**
+     * División de dos números
+     * @example
+     * n1 = 1, n2 = 3 => 0.333
+     * @param {*} n1 Número uno de la división
+     * @param {*} n2 Número dos de la división
+     * @returns n1 / n2
+     */
+    division: function(n1, n2) {
+        return n1 / n2;
+    }
+}
